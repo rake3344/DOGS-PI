f13d4f017a610a29321548dc089f0ccd11eea821
rake3344 <juan.agudelo17@udea.edu.co>
2023-04-27 19:29:44 -0500

    pagination

--- client/src/components/Paginate/Paginate.jsx
+++ client/src/components/Paginate/Paginate.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./Paginate.css"
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 export default function Paginate({ dogsPerPage, allDogs, paginate }) {
 
@@ -13,18 +15,23 @@ export default function Paginate({ dogsPerPage, allDogs, paginate }) {
 
 
     return (
-        <nav className='pagination'>
-            <ul>
-                {
-                    pageNumbers?.map(number => {
-                        return (
-                            <li key={number}>
-                                <button onClick={() => {paginate(number)}}>{number}</button>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+        // <nav className='pagination'>
+        //     <ul>
+        //         {
+        //             pageNumbers?.map(number => {
+        //                 return (
+        //                     <li key={number}>
+        //                         <button onClick={() => {paginate(number)}}>{number}</button>
+        //                     </li>
+        //                 )
+        //             })
+        //         }
+        //     </ul>
+        // </nav>
+        <nav className='paginate'>
+            <Stack spacing={2}>
+                <Pagination count={div} onChange={(e) => paginate(e.target.textContent)} />
+            </Stack>
         </nav>
     )
 }
