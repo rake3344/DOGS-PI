768592d28f2fc81a11b8ddb33bdd146bdd68118f
rake3344 <juan.agudelo17@udea.edu.co>
2023-07-19 11:58:30 -0500

    production backend added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import axios from 'axios'
-import store from './Redux/store.js'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+import store from "./Redux/store.js";
+import { Provider } from "react-redux";
 
-axios.defaults.baseURL = 'http://localhost:3001'
+// axios.defaults.baseURL = 'http://localhost:3001'
+axios.defaults.baseURL = "https://dogs-pi-production-0dfe.up.railway.app";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
